659bf8cc74ba9cd9cfa2e1d90399c9866b0f3c55
varshithvhegde <varshithvh@gmail.com>
2022-07-17 19:13:21 +0530

    splash screen edit

--- app/src/main/java/com/varshith/myblink/SplashScreen.java
+++ app/src/main/java/com/varshith/myblink/SplashScreen.java
@@ -3,16 +3,18 @@ package com.varshith.myblink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         Handler handler = new Handler(Looper.myLooper());
         handler.postDelayed(() -> startActivity(new Intent(SplashScreen.this, MainActivity.class)), 3000);
     }
